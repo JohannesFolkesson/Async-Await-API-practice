97d824f9259413c46ce1550ad5b0c4124cb2116f
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-10-22 11:50:41 +0200

    div

--- tv-show-finder/app.js
+++ tv-show-finder/app.js
@@ -11,6 +11,32 @@ btn.addEventListener('click', async () => {
     try{
         const response = await fetch(`https://api.tvmaze.com/search/shows?q=QUERY`)
         if(!response.ok) throw new error('Nätverksfel')
-            
+            const data = await response.json()
+        console.log(data)
+        displayShows(data);
+} catch (error) {
+    div.innerHTML = `<p style="color:red;">Fel: ${error.message}</p>`;
+}
+
+    });
+    function displayShows(shows) {
+        div.innerHTML("")
     }
-})
+    if (shows.length === 0) {
+        div.innerHTML = `<p "Inga serier hittades"'</p>`
+        return;
+    }
+
+    shows.forEach(item => {
+        const show = item.show;
+            const image = show.image ? show.image.medium : "https://via.placeholder.com/210x295?text=No+Image";
+        const showDiv = document.createElement('div');
+        showDiv.classList.add("show");
+            showDiv.innerHTML = `
+      <img src="${image}" alt="${show.name}">
+      <h2>${show.name}</h2>
+      <p>${show.genres.join(", ")}</p>
+      <a href="${show.url}" target="_blank">Mer info</a>
+    `;
+        div.appendChild(showDiv);
+    })
